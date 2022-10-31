ab66bf2025e5686b0df38f2098817f9d019c7ffa
qwer <greenforest249@gmail.com>
2022-10-31 17:05:00 +0200

    commit_3

--- main/urls.py
+++ main/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from main import views
+
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('store/', views.store, name='store'),
     path('admin/', admin.site.urls),
 ]
