1eb1c0bb651efdc9f2b8053eb7702805b5d82ada
qwer <greenforest249@gmail.com>
2022-11-25 00:32:34 +0200

    commit_6

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import logout, authenticate, login
 from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
 
-from authentication.forms import LoginForm
+from authentication.forms import LoginForm, RegisterForm
 
 
 # Create your views here.
@@ -16,11 +17,35 @@ def login_user(request):
             if user:
                 login(request, user)
                 return redirect('index')
-
+            else:
+                context = {
+                    'login_form': login_form,
+                    'attention': f'this username {username} is not found!'
+                }
+        else:
+            context = {
+                'login_form': login_form,
+            }
     return render(request, 'templates/auth/login.html', context)
 
-def register_user(request):
-    return render(request, 'templates/auth/register.html')
+
+class RegisterView(TemplateView):
+    template_name = 'templates/auth/register.html'
+
+    def get(self, request):
+        user_form = RegisterForm()
+        context = {'user_form': user_form}
+        return render(request, 'templates/auth/register.html', context)
+
+    def post(self, request):
+        user_form = RegisterForm(request.POST)
+        if user_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            login(request, user)
+            return redirect('index')
+
 
 def logout_user(request):
     logout(request)
